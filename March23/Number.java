37a743daf3ac12831d0ca820c460f89c015a371d
PavanDeeti <deetipavan55@gmail.com>
2025-03-24 11:41:00 +0530

    Add files via upload

---
+++ March23/Number.java
@@ -0,0 +1,14 @@
+import java.util.*;
+class Number{
+	public static void main(String args[]){
+		Scanner scan = new Scanner(System.in);
+		int x=scan.nextInt();
+		if(x>0){
+			System.out.println("Number is positive");
+		}
+		else {
+			System.out.println("Number is negative");
+		}
+
+	}
+}
